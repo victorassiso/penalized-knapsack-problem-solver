e3c6eaed3f99bc95644ee4431d3340271fe19f89
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-12 01:04:26 -0300

    Patch: main.cpp
    - Call constructive method and teste it's results

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "dataset.h"
+#include "solver.h"
 
 int main()
 {
-  Dataset dataset("instances/500/kpf_1_sum.txt");
+  Solver solver("instances/500/kpf_1_sum.txt", "config.json");
 
-  dataset.log_dataset();
+  Solution solution = solver.constructive();
+  solution.print();
+  std::cout<<"Total Prize: "<<solution.get_prize()<<std::endl;
+
+  int calculated_prize = solution.calculate_prize(solver.get_dataset());
+  std::cout<<"Calculated Prize: "<<calculated_prize<<std::endl;
 }
